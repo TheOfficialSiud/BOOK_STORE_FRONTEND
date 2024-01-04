cb7f202cd5863a315c73f6579248dd0566e8a59e
Sudhanshu Tripathi <sudhaanshutripathi@gmail.com>
2024-01-04 21:42:48 +0530

    Update ManageBooks.jsx

--- src/dashboard/ManageBooks.jsx
+++ src/dashboard/ManageBooks.jsx
@@ -6,27 +6,17 @@ import { Link } from 'react-router-dom';
 
 
 const ManageBooks = () => {
-  const[allBooks, setAllBooks] = useState([]);
-  useEffect(()=>{
-    fetch("https://book-store-backend-deployed.onrender.com/all-books")
-    .then(res=>res.json())
-    .then(data=>
-      // console.log(data);
-      setAllBooks(data));
-  },[])
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    fetch('https://book-store-backend-deployed.onrender.com/all-books')
+    .then(res => res.json())
+    .then(data => setAllBooks(data.slice(0, 6)))
+}, [])
+
 
   // delete a book
 
-  const handleDelete = (id)=>{  console.log(id);
-    fetch(`https://book-store-backend-deployed.onrender.com/book/${id}`,
-    {
-      method: "DELETE",
-    })
-    .then(res=>res.json())
-    .then(data=>{alert("Book Deleted Successfully")
-    // setAllBooks(data);
-  })
-  }
+
 
 
   return (
@@ -47,25 +37,35 @@ const ManageBooks = () => {
         </Table.Head>
 
         {
-          allBooks.map((book, index)=> <Table.Body className="divide-y" key={book._id}>
+          allBooks.map((book, index) => <Table.Body className="divide-y" key={book._id}>
             <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {index+1}
-            </Table.Cell>
-            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-              {book.bookTitle}
-            </Table.Cell>
-            <Table.Cell>{book.authorName}</Table.Cell>
-            <Table.Cell>{book.category}</Table.Cell>
-            <Table.Cell>$2999</Table.Cell>
-            <Table.Cell>
-              <Link to={`/admin/dashboard/edit-books/${book._id}`} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5">
-                Edit
-              </Link>
-              <button onClick={()=> handleDelete(book._id)} className='bg-red-600 px-4 py-1 font-semibold text-white rounded-sm'>Delete</button>
-            </Table.Cell>
-          </Table.Row>
-            </Table.Body>)
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {index + 1}
+              </Table.Cell>
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {book.bookTitle}
+              </Table.Cell>
+              <Table.Cell>{book.authorName}</Table.Cell>
+              <Table.Cell>{book.category}</Table.Cell>
+              <Table.Cell>$2999</Table.Cell>
+              <Table.Cell>
+                <Link to={`/admin/dashboard/edit-books/${book._id}`} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5">
+                  Edit
+                </Link>
+                <button onClick={() => {  
+                  fetch(`https://book-store-backend-deployed.onrender.com/book/${book._id}`,
+                    {
+                      method: "DELETE",
+                    })
+                    .then(res => res.json())
+                    .then(data => {
+                      alert("Book Deleted Successfully")
+                      // setAllBooks(data);
+                    })
+                }} className='bg-red-600 px-4 py-1 font-semibold text-white rounded-sm'>Delete</button>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>)
 
         }
 
